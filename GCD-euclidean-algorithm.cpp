7635901075907983a1c7374165489db4466b0ebe
hkonok <konokhabibullah@gmail.com>
2016-08-23 13:54:06 +0600

    GCD euclidean algorithm

---
+++ GCD-euclidean-algorithm.cpp
@@ -0,0 +1,32 @@
+/*
+ * GCD euclidean algorithm
+ */
+ #include <iostream>
+ #include <cstdio>
+ #include <cstring>
+ #include <cstdlib>
+ #include <utilitiy>
+ using namespace std;
+
+ int GCD(int a, int b){
+ 	int q,r,tmp;
+ 	if(b>a){
+ 		tmp = a;
+ 		a=b;
+ 		b=tmp;
+ 	}
+ 	while(a%b){
+ 		r=a%b;
+ 		a=b;
+ 		b=r;
+ 	}
+ 	return b;
+ }
+
+
+ int main(){
+ 	int a,b;
+ 	cin>>a>>b;
+ 	cout<<GCD(a, b)<<endl;
+ 	return 0;
+ }
