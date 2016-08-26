fb46ed1580703e1302943a1242170a0a4554183a
hkonok <konokhabibullah@gmail.com>
2016-08-26 16:19:38 +0600

    KMP modified.

--- knuth-morris-pratt.cpp
+++ knuth-morris-pratt.cpp
@@ -4,6 +4,8 @@
  #include <cstdio>
  #include <iostream>
  #include <cstdlib>
+ #include <cstring>
+ #include <string>
  using namespace std;
  #define N 100000
 
@@ -11,22 +13,65 @@
  int p[N];
 
  void pre_kmp(char *arr){
- 	int L=0;
+ 	int L=-1;
  	p[0] = -1;
  	for(int i=1; arr[i]; i++){
- 		while(L>=0 && arr[L]!= arr[i]){
+ 		while(L>=0 && arr[L+1]!=arr[i]){
  			L = p[L];
  		}
- 		L++;
- 		p[i] = L;
+
+ 		if(arr[L+1] == arr[i]){
+            L++;
+            p[i] = L;
+ 		}else{
+            p[i] = L;
+ 		}
  	}
  }
 
- void kmp(char *arr, char *inp){
+ void kmp(char *inp, char *arr, int len){
 
+ 	int L=-1;
+ 	for(int i=0; inp[i]; i++){
+ 		while(L>=0 && arr[L+1]!=inp[i]){
+ 			L = p[L];
+ 		}
+
+ 		if(arr[L+1] == inp[i]){
+            L++;
+ 		}
+
+ 		if(L==(len-1)){
+ 			cout<<"pos: "<<i<<endl;
+ 			for(int j=i-len+1; j<=i; j++){
+ 				cout<<inp[j];
+ 			}
+ 			cout<<endl;
+ 			L = p[L];
+ 		}
+
+ 	}
  }
 
  int main(){
+ 	char inp[N];
  	cin>>arr;
+ 	cin>>inp;
+
+
+
+ 	pre_kmp(arr);
+
+    for(int i=0; arr[i]; i++){
+        cout<<arr[i]<<" ";
+ 	}
+ 	cout<<endl;
+ 	for(int i=0; arr[i]; i++){
+        cout<<p[i]<<" ";
+ 	}
+ 	cout<<endl;
+
+
+ 	kmp(inp, arr, strlen(arr));
  	return 0;
  }
